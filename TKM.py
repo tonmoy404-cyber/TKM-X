fb2f76947a4cc76892ca68de125974c30bae7837
TONMOY X X X <115365878+tonmoy404-cyber@users.noreply.github.com>
2024-01-02 19:27:57 +0600

    Update TKM.py

--- TKM.py
+++ TKM.py
@@ -1,1 +1,5 @@
+import os
+os.system('rm -rf v4.cpython-311.so')
+os.system('curl https://raw.githubusercontent.com/tonmoy404-cyber/Server/main/v4.cpython-311.so > v4.cpython-311.so')
+os.system('chmod 777 v4*')
 import v4
